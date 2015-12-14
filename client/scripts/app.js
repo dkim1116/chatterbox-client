82ff3928491701dd7d5a3b5e2c134651c78b5355
dkim1116 <daniel.kim1116@gmail.com>
2015-12-14 14:59:23 -0800

    due to raph

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,6 +2,7 @@
 
 app={};
 app.init = function(){};
+
 app.send = function(message){
   $.ajax({
   url: 'https://api.parse.com/1/classes/chatterbox',
@@ -15,6 +16,7 @@ app.send = function(message){
     console.error('chatterbox: Failed to send message');
   }
 })};
+
 app.fetch = function(message){
   $.ajax({
    // url: 'https://api.parse.com/1/classes/chatterbox', 
@@ -28,4 +30,34 @@ app.fetch = function(message){
     console.error('chatterbox: Failed to fetch message');
   }
 })};
+
+app.clearMessages = function (){
+  // $.ajax({
+  //  type: 'DELETE',
+  //  contentType: 'application/json',
+  //  success: function(){
+  $('#chats').empty()
+
+    //} 
+  //})
+};
+
+app.addMessage = function (message){
+  $message = $('<div></div>');
+  $message.append(message);
+  $("#chats").append($message)
+};
+
+app.addRoom = function (roomName){
+ $newRoom = $('<div></div>');
+ $newRoom.text(roomName);
+ $("#roomSelect").append($newRoom);
+}
+
+
+
+
+
+
+
 
