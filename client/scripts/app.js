b6d10262d86c933eca8bf32d61791e787dd4cfe9
dkim1116 <daniel.kim1116@gmail.com>
2015-12-14 12:34:45 -0800

    First few tests passed

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,31 @@
 // YOUR CODE HERE:
 
+app={};
+app.init = function(){};
+app.send = function(message){
+  $.ajax({
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+    success: function (data){
+      console.log('chatterbox: Message sent');
+    },
+  error: function (data){
+    console.error('chatterbox: Failed to send message');
+  }
+})};
+app.fetch = function(message){
+  $.ajax({
+   // url: 'https://api.parse.com/1/classes/chatterbox', 
+  type: 'GET',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+    success: function (data){
+      console.log('chatterbox: Message fetched');
+    },
+  error: function (data){
+    console.error('chatterbox: Failed to fetch message');
+  }
+})};
+
