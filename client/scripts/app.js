88f7c83f5c7bbfed2610075fecadac69eb4ced03
dkim1116 <daniel.kim1116@gmail.com>
2015-12-14 15:45:09 -0800

    raph protection

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -42,22 +42,37 @@ app.clearMessages = function (){
   //})
 };
 
+//Passed in argument is an object
 app.addMessage = function (message){
-  $message = $('<div></div>');
-  $message.append(message);
+  $message = $('<div class="chat"></div>');
+
+  //Grab the 'text' key's value from the message object
+  $message.append(message.text);
   $("#chats").append($message)
+  $('#chats').append($('<div>testing</div>'))
 };
 
+//Passed in argument is a string
 app.addRoom = function (roomName){
  $newRoom = $('<div></div>');
- $newRoom.text(roomName);
+ 
+ //Since the roomName is a string itself, no need to grab it out of the key
+ $newRoom.append(roomName);
  $("#roomSelect").append($newRoom);
+};
+
+app.addFriend = function(){
+
 }
 
+var message = {
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};
 
-
-
-
-
+$('.refresh').on('Click', function(){
+  
+}
 
 
